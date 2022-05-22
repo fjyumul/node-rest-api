258157af9875218e83dd325c95b389095f98476a
Francis Jay Yumul <francis_jay_yumul@manulife.ca>
2022-05-22 18:36:53 +0800

    Added Error Handling + Refactor

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,19 +1,44 @@
 const workoutService = require('../services/workoutService');
 
 const getAllWorkouts = (req, res) => {
-    const allWorkouts = workoutService.getAllWorkouts();
-    res.send({ status: "Ok", data: allWorkouts});
+    try {
+      const allWorkouts = workoutService.getAllWorkouts();
+      res.send({ status: "Ok", data: allWorkouts});
+    } catch (error) {
+      res.status(error?.status || 500)
+      .send({status: "FAILED", data: { error: error?.message || error}});
+    }
 };
 
 const getOneWorkout = (req, res) => {
-    const workout = workoutService.getOneWorkout();
-    res.send("Get an existing workout");
+    const { workoutId } = req.params;
+
+    if (!workoutId)
+      return;
+
+    try {
+      const workout = workoutService.getOneWorkout(workoutId);
+      res.send({status: "Ok", data: workout});
+    } catch (error) {
+      res.status(error?.status || 500)
+      .send({status: "FAILED", data: { error: error?.message || error}});
+    }
+
 };
 
 const createNewWorkout = (req, res) => {
     const { body } = req;
 
-    if (!body.name || !body.mode || !body.equipment || !body.exercises || !body.trainerTips) return;
+    if (!body.name || !body.mode || !body.equipment || !body.exercises || !body.trainerTips) {
+      res.status(400)
+        .send({
+          status: "FAILED",
+          data: {
+              error: "One of the following keys is missing or is empty in request body:'name', 'mode', 'equipment', 'exercises', 'trainerTips'"
+          },
+        });
+        return;
+      }
 
     const newWorkout = {
         name: body.name,
@@ -23,18 +48,60 @@ const createNewWorkout = (req, res) => {
         trainerTips: body.trainerTips
     };
 
-    const createdWorkout = workoutService.createWorkout(newWorkout);
-    res.status(201).send({status: "Ok", data: createdWorkout});
+    try {
+      const createdWorkout = workoutService.createWorkout(newWorkout);
+      res.status(201).send({status: "Ok", data: createdWorkout});
+    } catch (error) {
+      res.status(error?.status || 500)
+      .send({status: "FAILED", data: { error: error?.message || error}});
+    }
+
 };
 
 const updateOneWorkout = (req, res) => {
-    const updatedWorkout = workoutService.updateOneWorkout();
-    res.send("Update an existing workout");
+    const {body, params: { workoutId } } = req;
+
+    if(!workoutId) {
+      res.status(400)
+        .send({
+          status: "FAILED",
+          data: { error: "Parameter ':workoutId' cannot be empty"}
+        });
+
+      return;
+    };
+
+    try {
+      const updatedWorkout = workoutService.updateOneWorkout(workoutId, body);
+      res.send({status: "Ok", data: updatedWorkout});
+    } catch (error) {
+      res.status(error?.status || 500)
+      .send({status: "FAILED", data: { error: error?.message || error}});
+    }
+
 }
 
 const deleteOneWorkout = (req, res) => {
-    const deletedWorkout = workoutService.deleteOneWorkout();
-    res.send("Delete an existing workout");
+    const { workoutId } = req.params;
+
+    if(!workoutId) {
+      res.status(400)
+        .send({
+          status: "FAILED",
+          data: { error: "Parameter ':workoutId' cannot be empty"}
+        });
+
+      return;
+    };
+
+    try {
+      workoutService.deleteOneWorkout(workoutId);
+      res.status(204).send({status: 'Ok'});
+    } catch (error) {
+      res.status(error?.status || 500)
+      .send({status: "FAILED", data: { error: error?.message || error}});
+    }
+
 };
 
 module.exports = {
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,42 +1,64 @@
-//This represents the workout model 
+//This represents the workout model
 const Workout = require('../database/workout');
-const { v4: uuid} = require('uuid');
-
+const { v4: uuid } = require('uuid');
 const Workout = require('../database/workout');
 
 const getAllWorkouts = () => {
+  try {
     const allWorkouts = Workout.getAllWorkouts();
     return allWorkouts;
+  } catch (error) {
+    throw error;
+  }
 };
 
-const getOneWorkout = () => {
-    return;
+const getOneWorkout = (workoutId) => {
+  try {
+    const workout = Workout.getOneWorkout(workoutId);
+    return workout;
+  } catch (error) {
+    throw error;
+  }
+
 };
 
 const createWorkout = (newWorkout) => {
-    const workoutToInsert = {
-        ...newWorkout,
-        id: uuid(),
-        createdAt: new Date().toLocaleString('en-US', { timezone: 'UTC'} ),
-        updatedAt: new Date().toLocaleString('en-US', { timezone: 'UTC'} ),
-    }
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString('en-US', { timezone: 'UTC' }),
+    updatedAt: new Date().toLocaleString('en-US', { timezone: 'UTC' }),
+  }
 
+  try {
     const createdWorkout = Workout.createNewWorkout(workoutToInsert);
     return createdWorkout;
+  } catch (error) {
+    throw error;
+  }
 };
 
-const updateOneWorkout = () => {
-    return;
+const updateOneWorkout = (workoutId, changes) => {
+  try {
+    const updatedWorkout = Workout.updateOneWorkout(workoutId, changes);
+    return updatedWorkout;
+  } catch (error) {
+    throw error;
+  }
 };
 
-const deleteOneWorkout = () => {
-    return;
+const deleteOneWorkout = (workoutId) => {
+  try {
+    Workout.deleteOneWorkout(workoutId);
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
-    getAllWorkouts,
-    getOneWorkout,
-    createWorkout,
-    updateOneWorkout,
-    deleteOneWorkout
+  getAllWorkouts,
+  getOneWorkout,
+  createWorkout,
+  updateOneWorkout,
+  deleteOneWorkout
 }
